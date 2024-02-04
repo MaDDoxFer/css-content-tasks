acce89b3b0a7c9bff985f78ff3e70d0d595ff3e6
Aleksey Eliseev <alekseyeliseev@ya.ru>
2024-02-04 14:28:24 +0300

    Начальный коммит

---
+++ __tests__/5-units.test.js
@@ -0,0 +1,34 @@
+const { test, expect } = require("@playwright/test");
+
+const settings = {
+  screenshot: {
+    type: "jpeg",
+    quality: 70,
+    fullPage: true,
+  },
+  viewport: {
+    width: 800,
+    height: 600,
+  },
+};
+
+let page;
+
+test.beforeAll(async ({ browser }, testInfo) => {
+  testInfo.snapshotPath = (name) => `${testInfo.file}-snapshots/${name}`;
+  page = await browser.newPage();
+  await page.goto("/5-units.html");
+  await page.setViewportSize(settings.viewport);
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.describe("Item Pricing Template", () => {
+  test("Layout should be equal to template", async () => {
+    const template = await page.locator("html").screenshot(settings.screenshot);
+
+    expect(template).toMatchSnapshot();
+  });
+});
